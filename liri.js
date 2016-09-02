2f3026bdd107ccb9b7fb316aba0a057001712295
Carlos Gonzalez <carlosgonzalez@Carloss-MacBook-Pro.local>
2016-09-02 13:16:28 -0400

    work in progress

--- liri.js
+++ liri.js
@@ -25,7 +25,6 @@ inquirer.prompt([
 
             //     console.log(tweets.length);
 
-
             for (var i = 0; tweets.length > i; i++) {
 
                 console.log(tweets[i].text);
@@ -39,41 +38,85 @@ inquirer.prompt([
 
     if (argument.files === 'spotify-this-song') {
 
-        spotifyData.search({
-            type: 'track',
-            query: 'baby one more time'
-        }, function (err, data) {
-            if (err) {
-                console.log('Error occurred: ' + err);
-                return;
-            }
+        inquirer.prompt([
+            {
+                type: "input",
+                message: 'Select a song',
+                name: "songName",
+    }
 
-//            console.log(JSON.stringify(data.tracks.items[0], null, 2));
-//            console.log(JSON.stringify(data.tracks.items[0].name));
-            console.log("");
-           console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
-            console.log("Song Title: " + data.tracks.items[0].name);
-            console.log("Song Preview: " + data.tracks.items[0].preview_url);
-            console.log("Song Album: " + data.tracks.items[0].album.name);
-            console.log("");
+]).then(function (song) {
+
+            spotifyData.search({
+                type: 'track',
+                query: song.songName,
+            }, function (err, data) {
+                if (err) {
+                    console.log('Error occurred: ' + err);
+                    return;
+                }
+
+                //            console.log(JSON.stringify(data.tracks.items[0], null, 2));
+                //            console.log(JSON.stringify(data.tracks.items[0].name));
+
+                console.log("");
+                console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+                console.log("Song Title: " + data.tracks.items[0].name);
+                console.log("Song Preview: " + data.tracks.items[0].preview_url);
+                console.log("Song Album: " + data.tracks.items[0].album.name);
+                console.log("");
 
 
-        });
+            });
 
-
+        })
     }
 
     if (argument.files === 'movie-this') {
 
-        request.get("http://www.omdbapi.com/?t=frozen&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-
-                console.log(JSON.parse(body)["Title"]);
-
-            }
-        })
-
-
+        console.log("");
+        inquirer.prompt([
+            {
+                type: "input",
+                message: 'Select a Movie',
+                name: "movieName",
     }
 
+]).then(function (movie) {
+
+
+
+            request.get("http://www.omdbapi.com/?t=" + movie.movieName + "+&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+
+                    console.log("Movie Name:-------- "+JSON.parse(body)["Title"]+"\n");
+                    
+                    console.log("Movie Year: "+JSON.parse(body)["Year"]+"\n");
+                    
+                    console.log("Movie imdbRating: "+JSON.parse(body)["imdbRating"]+"\n");
+                    
+                    console.log("This movie was produce in: "+JSON.parse(body)["Country"]+"\n");
+                    
+                     console.log("Movie Language:  "+JSON.parse(body)["Language"] +"\n");
+                    
+                     console.log("Plot:  "+JSON.parse(body)["Plot"]+"\n");
+                    
+                    console.log("Actors:  "+JSON.parse(body)["Actors"]+"\n");
+                    
+                    console.log("Rotten Tomato Rating:  "+JSON.parse(body)["tomatoRating"]+"\n");
+                    
+                    console.log("Rotten Tomato URL:  "+JSON.parse(body)["tomatoURL"]+"\n");
+
+
+
+                
+                    //Actors in the movie.
+                    //Rotten Tomatoes Rating.
+                    //Rotten Tomatoes URL.
+
+                }
+            })
+        })
+
+    }
 })
