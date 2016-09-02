f27eda87e5b3b9d523f610f83894425bd9e63ea2
Carlos Gonzalez <carlosgonzalez@Carloss-MacBook-Pro.local>
2016-09-02 15:04:54 -0400

    final upload

--- liri.js
+++ liri.js
@@ -3,8 +3,8 @@ var spotifyData = require('spotify');
 var request = require('request');
 var inquirer = require('inquirer');
 var fs = require('fs');
+var passingData = false;
 
-//var path = process.argv[2];
 
 console.log("");
 inquirer.prompt([
@@ -18,39 +18,104 @@ inquirer.prompt([
 ]).then(function (argument) {
 
     if (argument.files === 'my-tweets') {
+        selectTweets();
+
+    }
+
+    if (argument.files === 'spotify-this-song') {
+        selectSong(null, passingData);
+
+    }
+
+    if (argument.files === 'movie-this') {
+        selectMovie(null, passingData);
+
+    }
+
+    if (argument.files === 'do-what-it-says') {
+
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            //            console.log(data);
+
+            var dataArr = data.split(',');
+
+            switch (dataArr[0]) {
+
+            case 'my-tweets':
+                selectTweets();
+                break;
+
+            case 'spotify-this-song':
+                passingData = true;
+                selectSong(dataArr[1], passingData);
+                break;
+
+            case 'movie-this':
+
+                passingData = true;
+                selectMovie(dataArr[1], passingData)
+                break;
+
+            }
+        });
+    }
+
+    function selectTweets() {
+
         twitterData.twitterKeys.get('statuses/user_timeline', {
             count: 10
         }, function (error, tweets, response) {
             if (error) throw error;
+
             //    console.log(JSON.stringify(tweets, null, 3));
 
             //     console.log(tweets.length);
 
             for (var i = 0; tweets.length > i; i++) {
-
                 console.log(tweets[i].text);
                 console.log('\n')
-
             }
-
         });
-
     }
 
-    if (argument.files === 'spotify-this-song') {
+    function selectSong(passedSong, passingData) {
 
-        inquirer.prompt([
-            {
-                type: "input",
-                message: 'Select a song',
-                name: "songName",
-    }
+        if (passingData === false) {
+
+            inquirer.prompt([
+
+                {
+                    type: "input",
+                    message: 'Select a song data',
+                    name: "songName",
+                }
 
 ]).then(function (song) {
 
+                spotifyData.search({
+                    type: 'track',
+                    query: song.songName,
+                }, function (err, data) {
+                    if (err) {
+                        console.log('Error occurred: ' + err);
+                        return;
+                    }
+
+                    //            console.log(JSON.stringify(data.tracks.items[0], null, 2));
+                    //            console.log(JSON.stringify(data.tracks.items[0].name));
+                    console.log("");
+                    console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+                    console.log("Song Title: " + data.tracks.items[0].name);
+                    console.log("Song Preview: " + data.tracks.items[0].preview_url);
+                    console.log("Song Album: " + data.tracks.items[0].album.name);
+                    console.log("");
+                });
+            })
+        } else {
+
             spotifyData.search({
                 type: 'track',
-                query: song.songName,
+                query: passedSong,
             }, function (err, data) {
                 if (err) {
                     console.log('Error occurred: ' + err);
@@ -67,27 +132,48 @@ inquirer.prompt([
                 console.log("Song Album: " + data.tracks.items[0].album.name);
                 console.log("");
 
-
             });
-
-        })
+        }
     }
 
-    if (argument.files === 'movie-this') {
+    function selectMovie(passingMovieName, passingData) {
 
-        console.log("");
-        inquirer.prompt([
-            {
-                type: "input",
-                message: 'Select a Movie',
-                name: "movieName",
+        if (passingData === false) {
+            console.log("");
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: 'Select a Movie',
+                    name: "movieName",
     }
 
 ]).then(function (movie) {
+                request.get("http://www.omdbapi.com/?t=" + movie.movieName + "+&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
 
+                        console.log("Movie Name:-------- " + JSON.parse(body)["Title"] + "\n");
 
+                        console.log("Movie Year: " + JSON.parse(body)["Year"] + "\n");
 
-            request.get("http://www.omdbapi.com/?t=" + movie.movieName + "+&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+                        console.log("Movie imdbRating: " + JSON.parse(body)["imdbRating"] + "\n");
+
+                        console.log("This movie was produce in: " + JSON.parse(body)["Country"] + "\n");
+
+                        console.log("Movie Language:  " + JSON.parse(body)["Language"] + "\n");
+
+                        console.log("Plot:  " + JSON.parse(body)["Plot"] + "\n");
+
+                        console.log("Actors:  " + JSON.parse(body)["Actors"] + "\n");
+
+                        console.log("Rotten Tomato Rating:  " + JSON.parse(body)["tomatoRating"] + "\n");
+
+                        console.log("Rotten Tomato URL:  " + JSON.parse(body)["tomatoURL"] + "\n");
+                    }
+                })
+            })
+            
+        }else{
+            request.get("http://www.omdbapi.com/?t=" + passingMovieName + "+&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
                 if (!error && response.statusCode == 200) {
 
                     console.log("Movie Name:-------- " + JSON.parse(body)["Title"] + "\n");
@@ -108,28 +194,8 @@ inquirer.prompt([
 
                     console.log("Rotten Tomato URL:  " + JSON.parse(body)["tomatoURL"] + "\n");
 
-
-
                 }
             })
-        })
-
+        }
     }
-
-    if (argument.files === 'do-what-it-says') {
-
-        fs.readFile("random.txt", "utf8", function (error, data) {
-
-           
-            console.log(data);
-            
-            var dataArr = data.split(',');
-            
-            console.log(dataArr);
-
-        });
-
-    }
-
-
 })
