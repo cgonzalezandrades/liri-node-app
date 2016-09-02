38fc5e26711ee055469c9385f926cae37bbe120c
Carlos Gonzalez <carlosgonzalez@Carloss-MacBook-Pro.local>
2016-09-02 12:51:05 -0400

    Work in progress

--- liri.js
+++ liri.js
@@ -1,24 +1,22 @@
 var twitterData = require('./keys.js');
 var spotifyData = require('spotify');
+var request = require('request');
 var inquirer = require('inquirer');
 
 //var path = process.argv[2];
 
+console.log("");
 inquirer.prompt([
     {
         type: "list",
-        message:'Select a command',
-        choices:['my-tweets','spotify-this-song'],
+        message: 'Select a command',
+        choices: ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
         name: "files",
     }
-    
-    
-    
-]).then(function(argument) {
-    
+
+]).then(function (argument) {
 
     if (argument.files === 'my-tweets') {
-
         twitterData.twitterKeys.get('statuses/user_timeline', {
             count: 10
         }, function (error, tweets, response) {
@@ -36,150 +34,46 @@ inquirer.prompt([
             }
 
         });
-        
+
     }
-    
-    if(argument.files === 'spotify-this-song'){
-        
-        console.log('i');
-        
-         spotifyData.search({type: 'track',query: 'baby one more time'}, function (err, data) {
+
+    if (argument.files === 'spotify-this-song') {
+
+        spotifyData.search({
+            type: 'track',
+            query: 'baby one more time'
+        }, function (err, data) {
             if (err) {
                 console.log('Error occurred: ' + err);
                 return;
             }
 
-//                        console.log(JSON.stringify(data.tracks.items[0], null, 2));
-            console.log(JSON.stringify(data.tracks.items[0].name));
+//            console.log(JSON.stringify(data.tracks.items[0], null, 2));
+//            console.log(JSON.stringify(data.tracks.items[0].name));
+            console.log("");
+           console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+            console.log("Song Title: " + data.tracks.items[0].name);
+            console.log("Song Preview: " + data.tracks.items[0].preview_url);
+            console.log("Song Album: " + data.tracks.items[0].album.name);
+            console.log("");
 
 
         });
-        
-        
+
+
     }
-    
 
-//    switch (path) {
-//
-//    case 'my-tweets':
-//
-//        twitterData.twitterKeys.get('statuses/user_timeline', {
-//            count: 10
-//        }, function (error, tweets, response) {
-//            if (error) throw error;
-//            //    console.log(JSON.stringify(tweets, null, 3));
-//
-//            //     console.log(tweets.length);
-//
-//
-//            for (var i = 0; tweets.length > i; i++) {
-//
-//                console.log(tweets[i].text);
-//                console.log('\n')
-//
-//            }
-//
-//        });
-//
-//        break;
+    if (argument.files === 'movie-this') {
+
+        request.get("http://www.omdbapi.com/?t=frozen&y=&plot=short&tomatoes=true&r=json", function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                console.log(JSON.parse(body)["Title"]);
+
+            }
+        })
 
 
+    }
 
-
-
-        //switch (path) {
-        //
-        //case 'my-tweets':
-        //
-        //    twitterData.twitterKeys.get('statuses/user_timeline', {
-        //        count: 10
-        //    }, function (error, tweets, response) {
-        //        if (error) throw error;
-        //        //    console.log(JSON.stringify(tweets, null, 3));
-        //
-        //        //     console.log(tweets.length);
-        //
-        //
-        //        for (var i = 0; tweets.length > i; i++) {
-        //
-        //            console.log(tweets[i].text);
-        //            console.log('\n')
-        //
-        //        }
-        //
-        //    });
-        //
-        //    break;
-//--------------
-//    case 'spotify-this-song':
-//        spotifyData.search({
-//            type: 'track',
-//            query: 'baby one more time'
-//        }, function (err, data) {
-//            if (err) {
-//                console.log('Error occurred: ' + err);
-//                return;
-//            }
-//
-//            //            console.log(JSON.stringify(data.tracks.items[0], null, 2));
-//            console.log(JSON.stringify(data.tracks.items[0].name));
-//
-//
-//        });
-//    
-//    ---------
-//        break;
-
-
-//    }
 })
-
-
-
-//twitterData.twitterKeys.get(path, {
-//    count: 10
-//}, function (error, tweets, response) {
-//    if (error) throw error;
-//    //    console.log(JSON.stringify(tweets, null, 3));
-//
-//    //     console.log(tweets.length);
-//
-//
-//    for (var i = 0; tweets.length > i; i++) {
-//
-//        console.log(tweets[i].text);
-//        console.log('\n')
-//
-//    }
-//
-//});
-
-//spotifyData.search({
-//    type: 'track',
-//    query: 'baby one more time'
-//}, function (err, data) {
-//    if (err) {
-//        console.log('Error occurred: ' + err);
-//        return;
-//    }
-//
-//    //    console.log(JSON.stringify(data.tracks.items[0], null, 2));
-//    //    console.log(JSON.stringify(data.tracks.items[0].name));
-//
-//
-//});
-
-
-//
-//spotifyData.spotifyKeys.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', { limit: 10, offset: 20 }, function(err, data) {
-//  if (err) {
-//    console.error('Something went wrong!');
-//  } else {
-//    console.log(data.body);
-//  }
-//});
-
-
-
-//console.log(twitterData.twitterKeys.consumer_key);
-//console.log(twitterData.twitterKeys.consumer_secret);
